6b987f452fbdab39a9c0843b1326e86ffa3cd02d
DaddyBearSEA <michael@massagepractice101.com>
2020-07-16 19:35:41 -0700

    Submitting for Object Leterals

--- js/sales.js
+++ js/sales.js
@@ -1,24 +1,13 @@
 'use strict'
 
 
-// 1.	Stores the min/max hourly customers, and the average cookies per customer, in object properties
-// 2.	Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-// 3.	Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated set up a count per hour and apply that to a RETURN Array? and then to the HTML page. (math = avgCookies * randeomNumberOfCustomers = numberOfCookiesPurchased)
-// 4.	Store the results for each location in a separate array… perhaps as a property of the object representing that location. Set a key: value creating an Array with the properties of each store per hour. 
-// 5.	Display the values of each array as unordered lists in the browser
-// 6.	Calculating the sum of these hourly totals; 
-// Display the lists on sales.html. 
-
+// these methods will cause you issues - 5 objects??  total duplicate.  protoype method  - Roger H
 
 // =====  Starting with Seattle Store  ==========
 
-
-// 1.	Stores the min/max hourly customers, and the average cookies per customer, in object properties per Store Site - Done!
-// 2.	Uses a method of that object to generate a random number of customers per hour. Objects/Math/random will eventually pass a variable but each store is set to a Max Number.
-
-// var storeName = 'Seattle';
 // global variable for hours of operations
 var hoursOfOp = ['6 am', '7 am', '8 am', '9 am', '10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm']
+
 
 var storeStatistics = {
   storeName: 'Seattle',
@@ -41,15 +30,15 @@ var storeStatistics = {
   },
 
   outputStoreName: function () {
-    var nameOutput = document.getElementById('outputStoreName'); //parent
-    var nameStoreOutput = document.createElement('h3'); // child
+    var nameOutput = document.getElementById('outputStoreName-SEA'); //parent
+    var nameStoreOutput = document.createElement('li'); // child
     nameStoreOutput.textContent = this.storeName //data from obj
     nameOutput.appendChild(nameStoreOutput);
   },
 
   outputStoreStatistics: function () {
     for (var i = 0; i < this.cookieSoldArray.length; i++) {
-      var storeOutput = document.getElementById('storeStatistics-output'); // parent
+      var storeOutput = document.getElementById('storeStatistics-output-SEA'); // parent
       var storeNameList = document.createElement('li'); // child
       storeNameList.textContent = hoursOfOp[i] + ' ' + this.cookieSoldArray[i] //  data from object
       storeOutput.appendChild(storeNameList);
@@ -59,21 +48,16 @@ var storeStatistics = {
   },
 
   totalNumberOfCookies: function () {
-    // console.log('inside totalNumberOfCookies');
-    // console.log(this.cookieSoldArray);
     for (var i = 0; i < this.cookieSoldArray.length; i++){
       this.storesCookieTotalSales += this.cookieSoldArray[i];
-      // console.log(this.cookieSoldArray[i]);
-      // console.log('Total cookie amount: ' + this.storesCookieTotalSales);
-      
-    }
+     }
     
 
   },
   outputStoreTotalDailySales: function() {
-    var storeTotalSales = document.getElementById('storeStatistics-output'); // parent
+    var storeTotalSales = document.getElementById('storeStatistics-output-SEA'); // parent
     var storeTotalSalesOutput = document.createElement('li'); // child
-    storeTotalSalesOutput.textContent = 'Total Sales for the Day ' + this.storesCookieTotalSales;
+    storeTotalSalesOutput.textContent = 'Total: ' + this.storesCookieTotalSales;
     storeTotalSales.appendChild(storeTotalSalesOutput);
 
   }
@@ -88,8 +72,273 @@ storeStatistics.outputStoreName();
 storeStatistics.outputStoreStatistics();
 storeStatistics.outputStoreTotalDailySales();
 
+//========  New Store ======
 
-// thes methods will cause you issues - 5 objects??  total duplicate.  protoype method 
+var storeStatistics = {
+  storeName: 'Tokyo',
+  customerMin: 3,
+  customerMax: 24,
+  avgCookieSales: 1.2,
+  cookieSoldArray: [],
+  storesCookieTotalSales: 0,
+  randomCustomerNo: function () {
+    return Math.floor(Math.random() * (Math.ceil(this.customerMax) - Math.floor(this.customerMin)) + this.customerMin);
+  },
+
+  cookiesSoldPerHour: function () {
+
+    for (var i = 0; i < 14; i++) {
+      var cookiesSoldThisHour = this.randomCustomerNo() * this.avgCookieSales;
+      var roundedCookies = Math.floor(cookiesSoldThisHour);
+      this.cookieSoldArray[i] = roundedCookies;
+    }
+  },
+
+  outputStoreName: function () {
+    var nameOutput = document.getElementById('outputStoreName-TYO'); //parent
+    var nameStoreOutput = document.createElement('li'); // child
+    nameStoreOutput.textContent = this.storeName //data from obj
+    nameOutput.appendChild(nameStoreOutput);
+  },
+
+  outputStoreStatistics: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++) {
+      var storeOutput = document.getElementById('storeStatistics-output-TYO'); // parent
+      var storeNameList = document.createElement('li'); // child
+      storeNameList.textContent = hoursOfOp[i] + ' ' + this.cookieSoldArray[i] //  data from object
+      storeOutput.appendChild(storeNameList);
+
+    }
+
+  },
+
+  totalNumberOfCookies: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++){
+      this.storesCookieTotalSales += this.cookieSoldArray[i];
+     }
+    
+
+  },
+  outputStoreTotalDailySales: function() {
+    var storeTotalSales = document.getElementById('storeStatistics-output-TYO'); // parent
+    var storeTotalSalesOutput = document.createElement('li'); // child
+    storeTotalSalesOutput.textContent = 'Total: ' + this.storesCookieTotalSales;
+    storeTotalSales.appendChild(storeTotalSalesOutput);
+
+  }
+
+};
+
+// Make the calls for the object to run 
+
+storeStatistics.cookiesSoldPerHour();
+storeStatistics.totalNumberOfCookies();
+storeStatistics.outputStoreName();
+storeStatistics.outputStoreStatistics();
+storeStatistics.outputStoreTotalDailySales(); 
+
+
+
+//========  New Store ======
+
+var storeStatistics = {
+  storeName: 'Dubai',
+  customerMin: 11,
+  customerMax: 38,
+  avgCookieSales: 3.7,
+  cookieSoldArray: [],
+  storesCookieTotalSales: 0,
+  randomCustomerNo: function () {
+    return Math.floor(Math.random() * (Math.ceil(this.customerMax) - Math.floor(this.customerMin)) + this.customerMin);
+  },
+
+  cookiesSoldPerHour: function () {
+
+    for (var i = 0; i < 14; i++) {
+      var cookiesSoldThisHour = this.randomCustomerNo() * this.avgCookieSales;
+      var roundedCookies = Math.floor(cookiesSoldThisHour);
+      this.cookieSoldArray[i] = roundedCookies;
+    }
+  },
+
+  outputStoreName: function () {
+    var nameOutput = document.getElementById('outputStoreName-DXB'); //parent
+    var nameStoreOutput = document.createElement('li'); // child
+    nameStoreOutput.textContent = this.storeName //data from obj
+    nameOutput.appendChild(nameStoreOutput);
+  },
+
+  outputStoreStatistics: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++) {
+      var storeOutput = document.getElementById('storeStatistics-output-DXB'); // parent
+      var storeNameList = document.createElement('li'); // child
+      storeNameList.textContent = hoursOfOp[i] + ' ' + this.cookieSoldArray[i] //  data from object
+      storeOutput.appendChild(storeNameList);
+
+    }
+
+  },
+
+  totalNumberOfCookies: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++){
+      this.storesCookieTotalSales += this.cookieSoldArray[i];
+     }
+    
+
+  },
+  outputStoreTotalDailySales: function() {
+    var storeTotalSales = document.getElementById('storeStatistics-output-DXB'); // parent
+    var storeTotalSalesOutput = document.createElement('li'); // child
+    storeTotalSalesOutput.textContent = 'Total: ' + this.storesCookieTotalSales;
+    storeTotalSales.appendChild(storeTotalSalesOutput);
+
+  }
+
+};
+
+// Make the calls for the object to run 
+
+storeStatistics.cookiesSoldPerHour();
+storeStatistics.totalNumberOfCookies();
+storeStatistics.outputStoreName();
+storeStatistics.outputStoreStatistics();
+storeStatistics.outputStoreTotalDailySales(); 
+
+
+
+//========  New Store ======
+
+var storeStatistics = {
+  storeName: 'Paris',
+  customerMin: 20,
+  customerMax: 38,
+  avgCookieSales: 2.3,
+  cookieSoldArray: [],
+  storesCookieTotalSales: 0,
+  randomCustomerNo: function () {
+    return Math.floor(Math.random() * (Math.ceil(this.customerMax) - Math.floor(this.customerMin)) + this.customerMin);
+  },
+
+  cookiesSoldPerHour: function () {
+
+    for (var i = 0; i < 14; i++) {
+      var cookiesSoldThisHour = this.randomCustomerNo() * this.avgCookieSales;
+      var roundedCookies = Math.floor(cookiesSoldThisHour);
+      this.cookieSoldArray[i] = roundedCookies;
+    }
+  },
+
+  outputStoreName: function () {
+    var nameOutput = document.getElementById('outputStoreName-CDG'); //parent
+    var nameStoreOutput = document.createElement('li'); // child
+    nameStoreOutput.textContent = this.storeName //data from obj
+    nameOutput.appendChild(nameStoreOutput);
+  },
+
+  outputStoreStatistics: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++) {
+      var storeOutput = document.getElementById('storeStatistics-output-CDG'); // parent
+      var storeNameList = document.createElement('li'); // child
+      storeNameList.textContent = hoursOfOp[i] + ' ' + this.cookieSoldArray[i] //  data from object
+      storeOutput.appendChild(storeNameList);
+
+    }
+
+  },
+
+  totalNumberOfCookies: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++){
+      this.storesCookieTotalSales += this.cookieSoldArray[i];
+     }
+    
+
+  },
+  outputStoreTotalDailySales: function() {
+    var storeTotalSales = document.getElementById('storeStatistics-output-CDG'); // parent
+    var storeTotalSalesOutput = document.createElement('li'); // child
+    storeTotalSalesOutput.textContent = 'Total: ' + this.storesCookieTotalSales;
+    storeTotalSales.appendChild(storeTotalSalesOutput);
+
+  }
+
+};
+
+// Make the calls for the object to run 
+
+storeStatistics.cookiesSoldPerHour();
+storeStatistics.totalNumberOfCookies();
+storeStatistics.outputStoreName();
+storeStatistics.outputStoreStatistics();
+storeStatistics.outputStoreTotalDailySales(); 
+
+
+//========  New Store ======
+
+var storeStatistics = {
+  storeName: 'Lima',
+  customerMin: 2,
+  customerMax: 16,
+  avgCookieSales: 4.6,
+  cookieSoldArray: [],
+  storesCookieTotalSales: 0,
+  randomCustomerNo: function () {
+    return Math.floor(Math.random() * (Math.ceil(this.customerMax) - Math.floor(this.customerMin)) + this.customerMin);
+  },
+
+  cookiesSoldPerHour: function () {
+
+    for (var i = 0; i < 14; i++) {
+      var cookiesSoldThisHour = this.randomCustomerNo() * this.avgCookieSales;
+      var roundedCookies = Math.floor(cookiesSoldThisHour);
+      this.cookieSoldArray[i] = roundedCookies;
+    }
+  },
+
+  outputStoreName: function () {
+    var nameOutput = document.getElementById('outputStoreName-LIMA'); //parent
+    var nameStoreOutput = document.createElement('li'); // child
+    nameStoreOutput.textContent = this.storeName //data from obj
+    nameOutput.appendChild(nameStoreOutput);
+  },
+
+  outputStoreStatistics: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++) {
+      var storeOutput = document.getElementById('storeStatistics-output-LIMA'); // parent
+      var storeNameList = document.createElement('li'); // child
+      storeNameList.textContent = hoursOfOp[i] + ' ' + this.cookieSoldArray[i] //  data from object
+      storeOutput.appendChild(storeNameList);
+
+    }
+
+  },
+
+  totalNumberOfCookies: function () {
+    for (var i = 0; i < this.cookieSoldArray.length; i++){
+      this.storesCookieTotalSales += this.cookieSoldArray[i];
+     }
+    
+
+  },
+  outputStoreTotalDailySales: function() {
+    var storeTotalSales = document.getElementById('storeStatistics-output-LIMA'); // parent
+    var storeTotalSalesOutput = document.createElement('li'); // child
+    storeTotalSalesOutput.textContent = 'Total: ' + this.storesCookieTotalSales;
+    storeTotalSales.appendChild(storeTotalSalesOutput);
+
+  }
+
+};
+
+// Make the calls for the object to run 
+
+storeStatistics.cookiesSoldPerHour();
+storeStatistics.totalNumberOfCookies();
+storeStatistics.outputStoreName();
+storeStatistics.outputStoreStatistics();
+storeStatistics.outputStoreTotalDailySales(); 
+
+
+
 
 
 // ================  start of forms  ===========================
